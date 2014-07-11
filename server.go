04c28a6a2dfac28b9fa334b90335ff9be6579602
Alex Saskevich <alexhomel@ya.ru>
2014-07-11 22:03:39 +0300

    dependencies

--- server.go
+++ server.go
@@ -1,9 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "github.com/go-martini/martini"
 
 func main() {
-	fmt.Println("Hello")
+	m := martini.Classic()
+	m.Get("/", func() string {
+			return "Hello World"
+		})
+	m.Get("/test", func() string {
+			return "Test"
+		})
+	m.Run()
 }
