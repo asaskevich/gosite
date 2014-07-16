06c217dc419dd43f132e2e237dd5e57e8c1542f6
Alex Saskevich <alexhomel@ya.ru>
2014-07-16 16:00:09 +0300

    Error page

--- server.go
+++ server.go
@@ -5,6 +5,8 @@ import (
 	"github.com/martini-contrib/sessions"
 	"net/http"
 	"github.com/asaskevich/gosite/server"
+	"github.com/martini-contrib/render"
+	"strconv"
 )
 
 // Entry point for server
@@ -13,6 +15,8 @@ func main() {
 	// set up the session, and map it into martini
 	store := sessions.NewCookieStore([]byte("auth"))
 	m.Use(sessions.Sessions("asaskevich", store))
+	// render html templates from templates directory
+	m.Use(render.Renderer())
 	// Redirect from "/" to "/home" url
 	m.Get("/", func(w http.ResponseWriter, r *http.Request, session sessions.Session) string {
 			http.Redirect(w, r, "/home", http.StatusFound)
@@ -27,9 +31,15 @@ func main() {
 			} else {
 				// TODO
 				// Render template with error
-				return "SOME ERRORS WITH REQUEST"
+				http.Redirect(w, r, "/error/"+strconv.Itoa(result), http.StatusFound)
+				return "SOME ERRORS"
 			}
 			return "OK"
+		})
+	// Rendering error page
+	m.Get("/error/**", func(params martini.Params, r render.Render) {
+			result := params["_1"]
+			r.HTML(200, "error", result)
 		})
 	m.Run()
 }
